7b34340149d09b936eb485be1f0e86c0eca77857
RichardDan2k1 <ricdan2k1@gmail.com>
2022-09-30 00:09:45 +0530

    including state
    tried state and handling events

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
     <>
 
     <Navbar title="TextUtils" about="About TextUtils"/>
+    
+    <div className="container">
+    <TextForm heading="Enter the Text to analyze below"/>
+    </div>
     </>
     
   );
