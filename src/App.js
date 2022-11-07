8c1b5d709484628589943ee8d46675f524612f2d
RichardDan2k1 <ricdan2k1@gmail.com>
2022-11-08 00:20:17 +0530

    dark mode
    improved dark mode and added switch to toggle it

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import './App.css';
+//import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import React, {useState} from 'react';
 
 function App() {
+  const [mode, setMode] = useState('light');// whether dark mode enable or not
+  
+  const toggleMode= () =>{
+    if(mode==='light'){
+    setMode('dark');
+    document.body.style.backgroundColor ="#080d15";
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor ="white";
+    }
+  }
+  
   return (
     <>
-
-    <Navbar title="TextUtils" about="About TextUtils"/>
+    {/* <Navbar titele ="TextUtils" aboutText="About TextUtils" />
+    <Navbar/> */}
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}/>
     
     <div className="container">
-    <TextForm heading="Enter the Text to analyze below"/>
+    <TextForm heading="Enter the Text to analyze below" mode={mode} />
+    {/* <About/> */}
     </div>
     </>
     
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -27,12 +27,12 @@ export default function TextForm(props) {
     
   return (
     <>
-        <div className='container'>
+        <div className='container' style={{color: props.mode==='dark' ? 'white':'#080d15'}}>
         <h1>{props.heading}</h1>
 
         <div className="mb-3">
 
-        <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="8"></textarea>
+        <textarea className="form-control" value={text} style={{backgroundColor: props.mode==='dark' ? 'grey':'white' , color: props.mode==='dark' ? 'white':'#080d15'}} onChange={handleOnChange} id="myBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
         <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
@@ -40,12 +40,12 @@ export default function TextForm(props) {
       
        
         </div>
-    <div className="container my-3">
+    <div className="container my-3" style={{color: props.mode==='dark' ? 'white':'#080d15'}}>
         <h2>Your Text summary</h2>
         <p>{text.split(" ").length} words and {text.length} characters</p> 
         <p>{0.008*text.split(" ").length} minutes to read </p>
         <h2>Preview</h2>
-        <p>{text}</p>
+        <p>{text.length>0? text:"Enter something to preview here"}</p>
     </div>
     </>
   )
